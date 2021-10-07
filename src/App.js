542e5362009060b801bef0639d0167e48ae1d4e8
rinakulikova185 <irinakulikova0185@gmail.com>
2021-10-07 14:26:16 +0300

    provider moved in index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
-import { Provider } from 'react-redux';
 
 import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Sidebar from './components/sidebar/Sidebar';
-import store from './store/index';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Provider store={store}>
+   
       <Router>
       <Header />
       <div className='body'>
@@ -22,7 +20,7 @@ function App() {
         <Main/>
       </div>
     </Router>
-    </Provider>
+    
     
   );
 }
