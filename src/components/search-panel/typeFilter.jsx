4ec72e988dded7ea5f7f53adb4ff2379581655ad
Iana Chyrva <yanachirva@gmail.com>
2021-10-13 14:20:45 +0300

    Final edits

--- src/components/search-panel/typeFilter.jsx
+++ src/components/search-panel/typeFilter.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import { useDispatch} from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { changeSelectedType } from '../../store/moviesSlice';
 
 export const TypeFilter = () => {
-    const dispatch = useDispatch();
-    const onSelectValueGet = (e) => {
-        const selectedIndex = e.target.options.selectedIndex;
-        dispatch(changeSelectedType(e.target.options[selectedIndex].value));
-      };
-    return (
-        <select
-          className='searchForm-select'
-          required
-          onChange={(e) => {
-            onSelectValueGet(e);
-          }}
-        >
-          <option disabled selected>
-            Выберите тип
-          </option>
-          <option value='movie'>фильм</option>
-          <option value='series'>сериал</option>
-          <option value='all'>все</option>
-        </select>
-    )
-}
+  const dispatch = useDispatch();
+  const onSelectValueGet = (e) => {
+    const selectedIndex = e.target.options.selectedIndex;
+    dispatch(changeSelectedType(e.target.options[selectedIndex].value));
+  };
+  return (
+    <select
+      className='searchForm-select'
+      required
+      defaultValue={'default'}
+      onChange={(e) => {
+        onSelectValueGet(e);
+      }}
+    >
+      <option disabled value='default'>
+        Выберите тип
+      </option>
+      <option value='movie'>фильм</option>
+      <option value='series'>сериал</option>
+      <option value='all'>все</option>
+    </select>
+  );
+};
